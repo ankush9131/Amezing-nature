6ab453f1f3dff051c02ba17f8d28a19bdd7096c0
ankush9131 <128287897+ankush9131@users.noreply.github.com>
2023-04-17 20:05:39 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://treadmoney.in` + encodeURI(path as string)
+					`https://treadmoney.in/` + encodeURI(path as string)
 				}`,
 			},
 		};
